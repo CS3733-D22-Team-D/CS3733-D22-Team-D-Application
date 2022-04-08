e61cc2bb4001c81ef91575a2ebb791e33e1e92c8
Jacob <jrmadams02@gmail.com>
2022-04-07 20:54:29 -0400

    working on sound

--- src/main/java/edu/wpi/DapperDaemons/controllers/easterEggController.java
+++ src/main/java/edu/wpi/DapperDaemons/controllers/easterEggController.java
@@ -1,11 +1,39 @@
 package edu.wpi.DapperDaemons.controllers;
 
-import javafx.fxml.FXML;
-
-import javax.swing.text.html.ImageView;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
 public class easterEggController extends UIController {
-    @FXML
-    ImageView
 
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    super.initialize(location, resources);
+
+    playSound("easterEgg.wav");
+  }
+
+  public static synchronized void playSound(final String url) {
+    new Thread(
+            new Runnable() {
+              // The wrapper thread is unnecessary, unless it blocks on the
+              // Clip finishing; see comments.
+              public void run() {
+                try {
+                  Clip clip = AudioSystem.getClip();
+                  AudioInputStream inputStream =
+                      AudioSystem.getAudioInputStream(
+                          easterEggController.class.getResourceAsStream(
+                              "/resources/edu/wpi/DapperDaemons/assets" + url));
+                  clip.open(inputStream);
+                  clip.start();
+                } catch (Exception e) {
+                  System.err.println(e.getMessage());
+                }
+              }
+            })
+        .start();
+  }
 }
