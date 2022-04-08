761b2e77af6c4575fac3e61b2084ac455f9add1f
Cole Ouellette <82121011+KFChaos@users.noreply.github.com>
2022-04-07 23:28:52 -0400

    Uploaded new cropped images for floors l2-5. Location Map crashes on open still

--- src/main/java/edu/wpi/DapperDaemons/map/MapHandler.java
+++ src/main/java/edu/wpi/DapperDaemons/map/MapHandler.java
@@ -57,13 +57,15 @@ public class MapHandler {
       case 1:
         return "L1";
       case 2:
-        return "G";
-      case 3:
         return "1";
-      case 4:
+      case 3:
         return "2";
-      case 5:
+      case 4:
         return "3";
+      case 5:
+        return "4";
+      case 6:
+        return "5";
       default:
         return "ERROR";
     }
