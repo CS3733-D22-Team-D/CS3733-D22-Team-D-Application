98be89523cfb620daaf65bff409c1e3f6437a9f6
Jacob <jrmadams02@gmail.com>
2022-04-08 22:34:53 -0400

    this a mess, coming back later

--- src/main/java/edu/wpi/DapperDaemons/Main.java
+++ src/main/java/edu/wpi/DapperDaemons/Main.java
@@ -4,6 +4,7 @@ import edu.wpi.DapperDaemons.backend.DAOPouch;
 import edu.wpi.DapperDaemons.backend.SecurityController;
 import edu.wpi.DapperDaemons.backend.csvLoader;
 import edu.wpi.DapperDaemons.entities.Employee;
+import edu.wpi.DapperDaemons.wongSweeper.MinesweeperZN;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Scanner;
@@ -14,13 +15,44 @@ public class Main {
   private static final String username = "admin";
   private static final String password = "admin";
 
-  public static void main(String[] args) throws IOException, SQLException {
+  public static synchronized void startApp(String[] args) throws SQLException, IOException {
     csvLoader.loadAll();
     DAOPouch.init();
     Employee admin = DAOPouch.getEmployeeDAO().get("admin");
     SecurityController.getInstance().setUser(admin);
     App.launch(App.class, args);
+  }
 
+  public static synchronized void startWong(String[] args) {
+    MinesweeperZN.launch(MinesweeperZN.class, args);
+  }
+
+  public static void main(String[] args) throws IOException, SQLException {
+    Thread app =
+        new Thread(
+            new Runnable() {
+              @Override
+              public void run() {
+                try {
+                  startApp(args);
+                } catch (SQLException e) {
+                  e.printStackTrace();
+                } catch (IOException e) {
+                  e.printStackTrace();
+                }
+              }
+            });
+    Thread wong =
+        new Thread(
+            new Runnable() {
+
+              @Override
+              public void run() {
+                startWong(args);
+              }
+            });
+
+    app.start();
     // Backend stuff
     /*ORMController ormc = new ORMController();
     boolean running;
