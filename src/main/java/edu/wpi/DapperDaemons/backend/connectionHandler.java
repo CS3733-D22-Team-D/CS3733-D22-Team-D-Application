1c6c64186854931dcaed56f3b75c2f59f1ab2f51
Jacob <jrmadams02@gmail.com>
2022-04-10 12:35:45 -0400

    Waiting on answers on how to implement

--- src/main/java/edu/wpi/DapperDaemons/backend/connectionHandler.java
+++ src/main/java/edu/wpi/DapperDaemons/backend/connectionHandler.java
@@ -1,5 +1,8 @@
 package edu.wpi.DapperDaemons.backend;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,13 +12,12 @@ class connectionHandler {
 
   static {
     try {
-      Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
-      connection = DriverManager.getConnection("jdbc:derby:BaW_database;create = true");
+      Class.forName("org.apache.derby.jdbc.ClientDriver");
+      connection = DriverManager.getConnection("jdbc:derby://localhost:1527/BaW_Database");
     } catch (SQLException e) {
       //      System.out.println("Database already created, continuing");
     } catch (ClassNotFoundException e) {
-      //      System.out.println("Driver error, try making sure you don't have any other instances
-      // open!");
+      System.out.println("Driver error, try making sure you don't have any other instances open!");
     }
   }
 
@@ -24,4 +26,25 @@ class connectionHandler {
   static Connection getConnection() {
     return connection;
   }
+
+  static Connection switchToClientServer() throws IOException {
+    ServerSocket ss = new ServerSocket(1527);
+    Socket server = ss.accept();
+    Socket client = new Socket("localhost",1527);
+    return connection;
+  }
+
+  static Connection switchToEmbedded(){
+    try {
+      Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+      connection = DriverManager.getConnection("jdbc:derby:BaW_database;create = true");
+    } catch (SQLException e) {
+      //      System.out.println("Database already created, continuing");
+    } catch (ClassNotFoundException e) {
+      //      System.out.println("Driver error, try making sure you don't have any other instances
+      // open!");
+    }
+    return connection;
+  }
+
 }
