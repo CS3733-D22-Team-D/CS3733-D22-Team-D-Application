2804d268c677fcd3bd7978f4eb778dbe71c78ae3
Jacob <jrmadams02@gmail.com>
2022-04-10 23:01:04 -0400

    weather get icon

--- src/test/java/edu/wpi/DapperDaemons/backend/weatherTest.java
+++ src/test/java/edu/wpi/DapperDaemons/backend/weatherTest.java
@@ -2,14 +2,20 @@ package edu.wpi.DapperDaemons.backend;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 class weatherTest {
 
   @Test
   void getWeather() throws Exception {
-    String[] res = weather.getWeather("boston");
-    System.out.println(res[0]);
-    System.out.println(res[1]);
+    Float res = weather.getWeather("boston");
+    System.out.println(res);
   }
 }
