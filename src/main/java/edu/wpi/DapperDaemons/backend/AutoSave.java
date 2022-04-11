a4f01e617a138aa51810c2c0d62ccedf2a4b50fb
Jacob <jrmadams02@gmail.com>
2022-04-11 17:24:02 -0400

    AutoSave

--- src/main/java/edu/wpi/DapperDaemons/backend/AutoSave.java
+++ src/main/java/edu/wpi/DapperDaemons/backend/AutoSave.java
@@ -1,31 +1,25 @@
 package edu.wpi.DapperDaemons.backend;
 
-import javafx.application.Platform;
-import javafx.scene.image.Image;
-
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class AutoSave {
-    private static Timer autoSave;
+  private static Timer autoSave;
 
-    private AutoSave() {
-    }
+  private AutoSave() {}
 
-    /**
-     * @param interval-the time in minutes between each autosave
-     */
-    public static void start(int interval) {
-        if (autoSave != null) autoSave.cancel();
-        autoSave = new Timer();
-        autoSave.schedule(
-                new TimerTask() {
-                      @Override
-                      public void run() {
-                       csvSaver.saveAll();
-                      }
-                  },
-        interval* 60000L,
+  /** @param interval-the time in minutes between each autosave */
+  public static void start(int interval) {
+    if (autoSave != null) autoSave.cancel();
+    autoSave = new Timer();
+    autoSave.schedule(
+        new TimerTask() {
+          @Override
+          public void run() {
+            csvSaver.saveAll();
+          }
+        },
+        interval * 60000L,
         interval * 60000L);
-    }
+  }
 }
