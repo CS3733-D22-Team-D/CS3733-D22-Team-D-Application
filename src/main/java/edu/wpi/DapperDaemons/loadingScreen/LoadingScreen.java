49a8148a51a73b1068a2fc0d4704090e2334fb4b
Jacob <jrmadams02@gmail.com>
2022-04-12 16:51:01 -0400

    fixed print outs

--- src/main/java/edu/wpi/DapperDaemons/loadingScreen/LoadingScreen.java
+++ src/main/java/edu/wpi/DapperDaemons/loadingScreen/LoadingScreen.java
@@ -18,42 +18,36 @@ public class LoadingScreen {
 
   public void display(Runnable init, Runnable display) throws IOException {
     new Thread(
-            new Runnable() {
-              @Override
-              public void run() {
-                Platform.runLater(
-                    new Runnable() {
-                      @Override
-                      public void run() {
-                        try {
-                          Parent root =
-                              FXMLLoader.load(
-                                  Objects.requireNonNull(
-                                      LoadingScreen.class
-                                          .getClassLoader()
-                                          .getResource(
-                                              "edu/wpi/DapperDaemons/views/loadingScreen.fxml")));
-                          Scene scene = new Scene(root);
-                          primaryStage.setMinWidth(635);
-                          primaryStage.setMinHeight(510);
-                          primaryStage.setScene(scene);
-                          primaryStage.show();
-                        } catch (IOException e) {
-                          e.printStackTrace();
-                        }
-                      }
-                    });
-                new Thread(
-                        () -> {
-                          init.run();
-                          Platform.runLater(
-                              () -> {
-                                display.run();
-                                stop();
-                              });
-                        })
-                    .start();
-              }
+            () -> {
+              Platform.runLater(
+                  () -> {
+                    try {
+                      Parent root =
+                          FXMLLoader.load(
+                              Objects.requireNonNull(
+                                  LoadingScreen.class
+                                      .getClassLoader()
+                                      .getResource(
+                                          "edu/wpi/DapperDaemons/views/loadingScreen.fxml")));
+                      Scene scene = new Scene(root);
+                      primaryStage.setMinWidth(635);
+                      primaryStage.setMinHeight(510);
+                      primaryStage.setScene(scene);
+                      primaryStage.show();
+                    } catch (IOException e) {
+                      e.printStackTrace();
+                    }
+                  });
+              new Thread(
+                      () -> {
+                        init.run();
+                        Platform.runLater(
+                            () -> {
+                              display.run();
+                              stop();
+                            });
+                      })
+                  .start();
             })
         .start();
   }
