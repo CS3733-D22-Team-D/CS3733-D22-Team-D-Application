6795e692d511686ebd3cba42864fec1166f1dbf0
Chandler Garcia <crgarcia@wpi.edu>
2022-04-11 23:34:02 -0400

    Bug testing and realizing I deleted my previous code entirely

--- src/main/java/edu/wpi/DapperDaemons/map/pathfinder/PathfinderHandler.java
+++ src/main/java/edu/wpi/DapperDaemons/map/pathfinder/PathfinderHandler.java
@@ -1,25 +1,118 @@
 package edu.wpi.DapperDaemons.map.pathfinder;
 
-import com.jfoenix.controls.JFXComboBox;
-import javafx.fxml.FXML;
+import edu.wpi.DapperDaemons.backend.DAOPouch;
+import edu.wpi.DapperDaemons.controllers.MapController;
+import edu.wpi.DapperDaemons.entities.Location;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.StrokeLineCap;
 
+/** Creates a path on the map */
 public class PathfinderHandler {
-  @FXML private JFXComboBox<String> toLocation;
-  @FXML private JFXComboBox<String> fromLocation;
 
-  AStar ppPlanner;
+  private AnchorPane lineLayer;
+  private MapController controller;
+  private List<String> nodePath;
+  private List<List<Line>> lineFloorPlan;
+  private List<Location> locations;
 
-  public PathfinderHandler() {
-    try {
-      ppPlanner = new AStar();
-    } catch (Exception e) {
-      e.printStackTrace();
-      System.out.println("Was unable to find or access the locations or nodeConnections tables");
+  public PathfinderHandler(AnchorPane lineLayer, MapController controller) {
+    this.lineLayer = lineLayer;
+    this.controller = controller;
+  }
+
+  public void showPather(String startNode, String endNode) {
+    makeLinePath(startNode, endNode);
+    makeAllInVisible();
+  }
+
+  /**
+   * Populates the line list / line image with all of its nodes using the AStar ppPlaner
+   *
+   * @param startNode
+   * @param endNode
+   */
+  private void makeLinePath(String startNode, String endNode) {
+    lineLayer.getChildren().removeAll();
+
+    AStar ppPlanner = new AStar(); // The path plan planner
+    // Gives all nodeID's of the path
+    nodePath = ppPlanner.getPath(startNode, endNode);
+    locations = new ArrayList<>();
+    for (String node : nodePath) {
+      try {
+        locations.add(DAOPouch.getLocationDAO().get(node));
+      } catch (Exception e) {
+        e.printStackTrace();
+        System.out.println("Location not found");
+      }
+    }
+
+    double overflow = 0.0;
+    for (int i = 0; i < locations.size() - 1; i++) {
+      // Add a new line to the list of lines
+      System.out.println(
+              "Position " + locations.get(i).getNodeID() + " to " + locations.get(i + 1).getNodeID());
+      System.out.println(
+              "X Start : "
+                      + locations.get(i).getXcoord()
+                      + " Start Y: "
+                      + locations.get(i).getYcoord());
+      System.out.println(
+              "X End : "
+                      + locations.get(i + 1).getXcoord()
+                      + " End Y: "
+                      + locations.get(i + 1).getYcoord());
+      Line pathLine =
+              new Line(
+                      locations.get(i).getXcoord(),
+                      locations.get(i).getYcoord(),
+                      locations.get(i + 1).getXcoord(),
+                      locations.get(i + 1).getYcoord());
+      pathLine.setFill(Color.RED);
+      pathLine.setStroke(Color.RED);
+      pathLine.setStrokeWidth(6.0);
+      pathLine.setStrokeLineCap(StrokeLineCap.SQUARE);
+      double lineLength =
+              Math.sqrt(
+                      Math.pow(locations.get(i).getXcoord() + locations.get(i + 1).getXcoord(), 2)
+                              + Math.pow(locations.get(i).getYcoord() + locations.get(i + 1).getYcoord(), 2));
+
+      // Start is 0d 24d
+      double whiteSpace = 24;
+      double lineSpace = 32;
+      boolean chancla = true;
+      while (lineLength > 0) {
+        if (chancla) {
+          pathLine.getStrokeDashArray().add(lineSpace);
+          lineLength -= lineSpace;
+          overflow = Math.abs(lineLength);
+        } else {
+          pathLine.getStrokeDashArray().add(whiteSpace);
+          lineLength -= whiteSpace;
+          overflow = 0;
+        }
+      }
+      lineLayer.getChildren().add(pathLine);
     }
   }
 
-  @FXML
-  public void showPath() {
-    if (!toLocation.getValue().isEmpty() && !toLocation.getValue().isEmpty()) {}
+  public void makeAllInVisible() {
+    lineLayer.getChildren().forEach(c -> c.setVisible(false));
+  }
+
+  public void filterByFloor(String floor) {
+    makeAllInVisible();
+    for (int i = 0;
+         i < locations.size() - 1;
+         i++) { // for every child, add make the locations on this floor visible
+      if (locations.get(i).getFloor().equals(floor)) {
+        lineLayer.getChildren().get(i).setVisible(true);
+      }
+    }
   }
 }
+
