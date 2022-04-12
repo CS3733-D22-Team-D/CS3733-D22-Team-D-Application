d1e768e330915204fb59161c6ef41dcb52a429ec
Jacob <jrmadams02@gmail.com>
2022-04-11 20:36:45 -0400

    added admin admin, and staff staff

--- src/main/java/edu/wpi/DapperDaemons/controllers/LoginController.java
+++ src/main/java/edu/wpi/DapperDaemons/controllers/LoginController.java
@@ -42,8 +42,13 @@ public class LoginController extends AppController {
   @FXML
   void login() throws Exception {
     Employee admin = DAOPouch.getEmployeeDAO().get("admin");
-    if (username.getText().equals("") && password.getText().equals("")) {
+    if (username.getText().equals("admin") && password.getText().equals("admin")) {
       SecurityController.setUser(admin);
+      switchScene("default.fxml", 635, 510);
+      return;
+    } else if (username.getText().equals("staff") && password.getText().equals("staff")) {
+      Employee staff = DAOPouch.getEmployeeDAO().get("staff");
+      SecurityController.setUser(staff);
       switchScene("default.fxml", 635, 510);
       return;
     } else if (username.getText().equals("rfid") && password.getText().equals("rfid")) {
