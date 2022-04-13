9b5766db51762955f05548124dec9243f8dd7877
Jacob <jrmadams02@gmail.com>
2022-04-12 22:41:32 -0400

    user settings pfp and names

--- src/main/java/edu/wpi/DapperDaemons/controllers/UserSettingsController.java
+++ src/main/java/edu/wpi/DapperDaemons/controllers/UserSettingsController.java
@@ -1,20 +1,55 @@
 package edu.wpi.DapperDaemons.controllers;
 
+import edu.wpi.DapperDaemons.backend.DAOPouch;
+import edu.wpi.DapperDaemons.backend.SecurityController;
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.Objects;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 
 public class UserSettingsController extends UIController {
 
   @FXML private Pane BGContainer;
   @FXML private ImageView BGImage;
 
+  @FXML private Circle profilePic1;
+
+  @FXML private Text accountName1;
+  @FXML private Text accountUserName;
+
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     super.initialize(location, resources);
 
+    String employeeName =
+        SecurityController.getUser().getFirstName()
+            + " "
+            + SecurityController.getUser().getLastName();
+    accountName1.setText(employeeName);
+    try {
+      accountUserName.setText(
+          DAOPouch.getAccountDAO().get(SecurityController.getUser().getNodeID()).getAttribute(1));
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+
+    profilePic1.setFill(
+        new ImagePattern(
+            new Image(
+                Objects.requireNonNull(
+                    getClass()
+                        .getClassLoader()
+                        .getResourceAsStream(
+                            "edu/wpi/DapperDaemons/profilepictures/"
+                                + SecurityController.getUser().getNodeID()
+                                + ".png")))));
     bindImage(BGImage, BGContainer);
   }
 }
