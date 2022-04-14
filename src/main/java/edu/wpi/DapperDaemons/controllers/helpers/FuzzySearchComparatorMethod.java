aac4076cb216f2d62828afeb75512844160ca6f8
Cole Ouellette <82121011+KFChaos@users.noreply.github.com>
2022-04-14 17:44:53 -0400

    Fixed home page working under restructured frontend

--- src/main/java/edu/wpi/DapperDaemons/controllers/helpers/FuzzySearchComparatorMethod.java
+++ src/main/java/edu/wpi/DapperDaemons/controllers/helpers/FuzzySearchComparatorMethod.java
@@ -4,8 +4,8 @@ import me.xdrop.fuzzywuzzy.FuzzySearch;
 
 public class FuzzySearchComparatorMethod implements AutoCompleteFuzzy.AutoCompleteComparator {
 
-    @Override
-    public boolean matches(String typedText, Object objectToCompare) {
-        return FuzzySearch.weightedRatio(typedText, objectToCompare.toString()) > 30;
-    }
+  @Override
+  public boolean matches(String typedText, Object objectToCompare) {
+    return FuzzySearch.weightedRatio(typedText, objectToCompare.toString()) > 30;
+  }
 }
