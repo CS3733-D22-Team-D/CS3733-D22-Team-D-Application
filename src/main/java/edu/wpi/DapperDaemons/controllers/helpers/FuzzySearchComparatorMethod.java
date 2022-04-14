c991b7f9fe21faa3dee83d67ecef606fda4f490a
Chandler Garcia <crgarcia@wpi.edu>
2022-04-14 01:47:20 -0400

    Fuzzy Search should be done

---
+++ src/main/java/edu/wpi/DapperDaemons/controllers/helpers/FuzzySearchComparatorMethod.java
@@ -0,0 +1,14 @@
+package edu.wpi.DapperDaemons.controllers.helpers;
+
+import me.xdrop.fuzzywuzzy.FuzzySearch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FuzzySearchComparatorMethod implements AutoCompleteFuzzy.AutoCompleteComparator {
+
+    @Override
+    public boolean matches(String typedText, Object objectToCompare) {
+        return FuzzySearch.weightedRatio(typedText, objectToCompare.toString()) > 30;
+    }
+}
