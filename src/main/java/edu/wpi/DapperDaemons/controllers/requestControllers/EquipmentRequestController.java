80d6db58a4c9656537ca68ee472f63a4a48c69c5
TK-Evans <101879445+TK-Evans@users.noreply.github.com>
2022-04-19 17:43:08 -0400

    fixed controllers and fxml apges

--- src/main/java/edu/wpi/DapperDaemons/controllers/requestControllers/EquipmentRequestController.java
+++ src/main/java/edu/wpi/DapperDaemons/controllers/requestControllers/EquipmentRequestController.java
@@ -202,7 +202,7 @@ public class EquipmentRequestController extends ParentController {
                       equipment.getNodeID(),
                       equipmentType,
                       cleanStatus,
-                      dateNeeded.getValue().toString()));
+                      dateStr));
           // check if user has permission
           if (!hadClearance) {
             showError("You do not have permission to do this.");
