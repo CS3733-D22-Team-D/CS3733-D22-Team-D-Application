e32e5c061d7e71278e8cf8a32a1cfdd135df5bd9
SamKarkache <93103617+SamKarkache@users.noreply.github.com>
2022-04-19 20:23:02 -0400

    added jar back, added run api button to about us page

--- src/main/java/edu/wpi/DapperDaemons/controllers/AboutUsController.java
+++ src/main/java/edu/wpi/DapperDaemons/controllers/AboutUsController.java
@@ -2,9 +2,13 @@ package edu.wpi.DapperDaemons.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 
 public class AboutUsController extends ParentController {
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {}
+
+  @FXML
+  public void runAPI() {}
 }
