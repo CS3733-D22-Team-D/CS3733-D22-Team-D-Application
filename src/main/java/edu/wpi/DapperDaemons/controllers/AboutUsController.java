48909bba8e686d20dec0f5c65cf60d9f75f9def5
SamKarkache <93103617+SamKarkache@users.noreply.github.com>
2022-04-19 20:45:34 -0400

    Trying to fix API database issues

--- src/main/java/edu/wpi/DapperDaemons/controllers/AboutUsController.java
+++ src/main/java/edu/wpi/DapperDaemons/controllers/AboutUsController.java
@@ -1,5 +1,7 @@
 package edu.wpi.DapperDaemons.controllers;
 
+import edu.wpi.cs3733.D22.teamD.API.ServiceException;
+import edu.wpi.cs3733.D22.teamD.API.StartAPI;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
@@ -10,5 +12,18 @@ public class AboutUsController extends ParentController {
   public void initialize(URL location, ResourceBundle resources) {}
 
   @FXML
-  public void runAPI() {}
+  public void runAPI() {
+    StartAPI api;
+    try {
+      api = new StartAPI();
+    } catch (ServiceException e) {
+      System.err.println("Something went wrong with the API");
+      return;
+    }
+    try {
+      api.run(0, 0, 800, 500, "edu/wpi/DapperDaemons/assets/themeBlue.css", "FSERV00101");
+    } catch (ServiceException e) {
+      System.err.println("A service error occurred");
+    }
+  }
 }
