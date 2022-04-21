38d2a4b600fb93eebe497e36c8cc4b5688a9f291
SamKarkache <93103617+SamKarkache@users.noreply.github.com>
2022-04-20 21:33:57 -0400

    testing API

--- src/main/java/edu/wpi/DapperDaemons/controllers/AboutUsController.java
+++ src/main/java/edu/wpi/DapperDaemons/controllers/AboutUsController.java
@@ -1,7 +1,6 @@
 package edu.wpi.DapperDaemons.controllers;
 
 import edu.wpi.cs3733.D22.teamD.API.*;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -11,9 +10,10 @@ public class AboutUsController extends ParentController {
   public void initialize(URL location, ResourceBundle resources) {}
 
   public void startAPI() {
+    StartAPI api = new StartAPI();
     try {
-      StartAPI.appLaunch();
-    } catch (IOException e) {
+      api.run(0, 0, 500, 800, "edu/wpi/DapperDaemons/assets/themeDark.css", "null");
+    } catch (Exception e) {
       System.err.println("API BROKE");
     }
   }
