55f24125a418d7582b297418704fc2378419a61c
SamKarkache <93103617+SamKarkache@users.noreply.github.com>
2022-04-21 20:45:51 -0400

    Added controller class and FXML for landing page

---
+++ src/main/java/edu/wpi/DapperDaemons/controllers/APILandingController.java
@@ -0,0 +1,14 @@
+package edu.wpi.DapperDaemons.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class APILandingController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
