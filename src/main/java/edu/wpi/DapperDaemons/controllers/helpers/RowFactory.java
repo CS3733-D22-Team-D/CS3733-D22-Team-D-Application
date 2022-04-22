d1eee625d5f1f36641b8cafc6580782f9fb6c9f1
Jacob <jrmadams02@gmail.com>
2022-04-22 14:43:37 -0400

    RowFactory and Table

---
+++ src/main/java/edu/wpi/DapperDaemons/controllers/helpers/RowFactory.java
@@ -0,0 +1,38 @@
+package edu.wpi.DapperDaemons.controllers.helpers;
+
+import edu.wpi.DapperDaemons.App;
+import edu.wpi.DapperDaemons.entities.TableObject;
+import edu.wpi.DapperDaemons.tables.TableHandler;
+import edu.wpi.DapperDaemons.tables.TableHelper;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+
+public class RowFactory {
+
+    private RowFactory(){}
+
+    public static HBox createRow(TableObject type, int tableNum){
+        HBox row = new HBox();
+        try {
+            row = FXMLLoader.load(
+                    Objects.requireNonNull(App.class.getResource("views/" + "row" + ".fxml")));
+        } catch(IOException ignored){}
+        List<Object> attributes = TableHelper.getDataList(type, tableNum);
+        for(Object attr: attributes){
+            if(attr instanceof Node) row.getChildren().add((Node)attr);
+            if(attr instanceof Enum) row.getChildren().add(new ComboBox<>());//figure this out
+            else{
+                row.getChildren().add(new Text(attr.toString()));
+            }
+        }
+        return row;
+    }
+}
