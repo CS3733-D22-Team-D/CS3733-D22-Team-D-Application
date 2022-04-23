b7d77a1ed029ffd3cf661cb1f75cf088d6e908db
SamKarkache <93103617+SamKarkache@users.noreply.github.com>
2022-04-22 20:21:18 -0400

    team z api does not seem to have employees or locations so the converters were removed

--- src/main/java/edu/wpi/DapperDaemons/APIConverters/TeamZLocationConverter.java
+++
@@ -1,7 +0,0 @@
-package edu.wpi.DapperDaemons.APIConverters;
-
-/**
- * Allows for conversion between our Location Objects and the Location Objects Present in team Z's API
- */
-public class TeamZLocationConverter {
-}
