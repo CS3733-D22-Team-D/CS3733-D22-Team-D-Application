7157f217cf7fd9d9059b8d2a279a4a3d81a0ee35
Gibbeh <49353685+Gibbeh@users.noreply.github.com>
2022-04-25 22:13:29 -0400

    ground work

---
+++ src/main/java/edu/wpi/DapperDaemons/controllers/NotificationsPageController.java
@@ -0,0 +1,16 @@
+package edu.wpi.DapperDaemons.controllers;
+
+import edu.wpi.DapperDaemons.entities.requests.Request;
+import edu.wpi.DapperDaemons.tables.Table;
+import edu.wpi.DapperDaemons.tables.TableHelper;
+import javafx.collections.FXCollections;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NotificationsPageController extends ParentController{
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
