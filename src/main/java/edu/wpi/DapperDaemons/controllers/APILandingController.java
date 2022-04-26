0283f9891fbd87a70879760d83e2b1d6b7c0206d
SamKarkache <93103617+SamKarkache@users.noreply.github.com>
2022-04-25 20:34:56 -0400

    changes css for team d, uncommented stuff

--- src/main/java/edu/wpi/DapperDaemons/controllers/APILandingController.java
+++ src/main/java/edu/wpi/DapperDaemons/controllers/APILandingController.java
@@ -1,8 +1,15 @@
 package edu.wpi.DapperDaemons.controllers;
 
+import edu.wpi.DapperDaemons.APIConverters.ExternalReqConverter;
+import edu.wpi.DapperDaemons.APIConverters.SanitationReqConverter;
 import edu.wpi.DapperDaemons.backend.DAOPouch;
 import edu.wpi.DapperDaemons.entities.requests.PatientTransportRequest;
 import edu.wpi.DapperDaemons.entities.requests.SanitationRequest;
+import edu.wpi.cs3733.D22.teamD.API.*;
+import edu.wpi.cs3733.D22.teamD.entities.LocationObj;
+import edu.wpi.cs3733.D22.teamD.request.SanitationIRequest;
+import edu.wpi.cs3733.D22.teamZ.api.API;
+import edu.wpi.cs3733.D22.teamZ.api.entity.ExternalTransportRequest;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
@@ -10,12 +17,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Paint;
 
-// import edu.wpi.cs3733.D22.teamD.API.*;
-// import edu.wpi.cs3733.D22.teamD.entities.LocationObj;
-// import edu.wpi.cs3733.D22.teamD.request.SanitationIRequest;
-// import edu.wpi.cs3733.D22.teamZ.api.API;
-// import edu.wpi.cs3733.D22.teamZ.api.entity.ExternalTransportRequest;
 public class APILandingController implements Initializable {
 
   @FXML private TextField teamDLoc;
@@ -64,10 +67,10 @@ public class APILandingController implements Initializable {
    * @return true if the location is present in the API database
    */
   public boolean isInLocationDatabase(String locID) {
-    //    LocationAPI locationAPI = new LocationAPI();
-    //    for (LocationObj loc : locationAPI.getAllLocations()) {
-    //      if (loc.getNodeID().equals(locID)) return true;
-    //    }
+    LocationAPI locationAPI = new LocationAPI();
+    for (LocationObj loc : locationAPI.getAllLocations()) {
+      if (loc.getNodeID().equals(locID)) return true;
+    }
     return false;
   }
 
@@ -90,88 +93,88 @@ public class APILandingController implements Initializable {
 
   /** Starts Team D Sanitation Request API (ours) */
   public void startTeamDApi() {
-    //    if (!isInLocationDatabase(teamDLoc.getText().trim())) {
-    //      errorLabel.setText("Error Invalid Location ID");
-    //      errorLabel.setTextFill(Paint.valueOf("EF5353"));
-    //      return;
-    //    }
-    //    destIDTeamD = teamDLoc.getText();
-    //    teamDLoc.setText("");
-    //    errorLabel.setText("");
-    //    StartAPI api = new StartAPI();
-    //    try {
-    //      api.run(
-    //          (int) zOrigin.getScene().getWindow().getX(),
-    //          (int) zOrigin.getScene().getWindow().getY(),
-    //          (int) zOrigin.getScene().getWindow().getHeight(),
-    //          (int) zOrigin.getScene().getWindow().getWidth(),
-    //          "edu/wpi/DapperDaemons/assets/themeBlue.css",
-    //          destIDTeamD);
-    //    } catch (Exception e) {
-    //      errorLabel.setText("Team-D API Broke");
-    //      return;
-    //    }
-    //    errorLabel.setText("You may have unsaved requests!");
-    //    errorLabel.setTextFill(Paint.valueOf("EF5353"));
-    //    dSave.setVisible(true);
+    if (!isInLocationDatabase(teamDLoc.getText().trim())) {
+      errorLabel.setText("Error Invalid Location ID");
+      errorLabel.setTextFill(Paint.valueOf("EF5353"));
+      return;
+    }
+    destIDTeamD = teamDLoc.getText();
+    teamDLoc.setText("");
+    errorLabel.setText("");
+    StartAPI api = new StartAPI();
+    try {
+      api.run(
+          (int) zOrigin.getScene().getWindow().getX(),
+          (int) zOrigin.getScene().getWindow().getY(),
+          (int) zOrigin.getScene().getWindow().getHeight(),
+          (int) zOrigin.getScene().getWindow().getWidth(),
+          "edu/wpi/DapperDaemons/assets/buttons.css",
+          destIDTeamD);
+    } catch (Exception e) {
+      errorLabel.setText("Team-D API Broke");
+      return;
+    }
+    errorLabel.setText("You may have unsaved requests!");
+    errorLabel.setTextFill(Paint.valueOf("EF5353"));
+    dSave.setVisible(true);
   }
 
   /** Saves all requests (that do not already exist) from the API to the program database */
   public void databaseSaverTeamD() {
-    //    dSave.setVisible(false);
-    //    SanitationReqAPI sanitationReqAPI = new SanitationReqAPI();
-    //    for (SanitationIRequest iReq : sanitationReqAPI.getAllRequests()) {
-    //      if (!checkIfSanitationReqExists(SanitationReqConverter.convert(iReq)))
-    //        DAOPouch.getSanitationRequestDAO().add(SanitationReqConverter.convert(iReq));
-    //    }
-    //    errorLabel.setText("Changes Saved!");
-    //    errorLabel.setTextFill(Paint.valueOf("00FF00"));
+    dSave.setVisible(false);
+    SanitationReqAPI sanitationReqAPI = new SanitationReqAPI();
+    for (SanitationIRequest iReq : sanitationReqAPI.getAllRequests()) {
+      if (!checkIfSanitationReqExists(SanitationReqConverter.convert(iReq)))
+        DAOPouch.getSanitationRequestDAO().add(SanitationReqConverter.convert(iReq));
+    }
+    errorLabel.setText("Changes Saved!");
+    errorLabel.setTextFill(Paint.valueOf("00FF00"));
   }
 
   /** Starts Team-Z's External Patient Request */
   public void startTeamZApi() {
-    //    if (!isInLocationDatabase(zDest.getText().trim())
-    //        || !isInLocationDatabase(zOrigin.getText().trim())) {
-    //      zErrorLabel.setText("Error Invalid Location ID");
-    //      zErrorLabel.setTextFill(Paint.valueOf("EF5353"));
-    //      return;
-    //    }
-    //    teamZOriginID = zOrigin.getText();
-    //    teamZDestinationID = zDest.getText();
-    //    zOrigin.setText("");
-    //    zDest.setText("");
-    //
-    //    API api = new API();
-    //    try {
-    //      api.run(
-    //          (int) zOrigin.getScene().getWindow().getX(),
-    //          (int) zOrigin.getScene().getWindow().getY(),
-    //          (int) zOrigin.getScene().getWindow().getWidth(),
-    //          (int) zOrigin.getScene().getWindow().getHeight(),
-    //          "edu/wpi/DapperDaemons/assets/teamZAPI.css",
-    //          teamZDestinationID,
-    //          teamZOriginID);
-    //    } catch (Exception e) {
-    //      System.err.println("Team Z's API Broke");
-    //      return;
-    //    }
-    //    zErrorLabel.setText("You may have unsaved requests!");
-    //    zErrorLabel.setTextFill(Paint.valueOf("EF5353"));
-    //    zSave.setVisible(true);
+    if (!isInLocationDatabase(zDest.getText().trim())
+        || !isInLocationDatabase(zOrigin.getText().trim())) {
+      zErrorLabel.setText("Error Invalid Location ID");
+      zErrorLabel.setTextFill(Paint.valueOf("EF5353"));
+      return;
+    }
+    teamZOriginID = zOrigin.getText();
+    teamZDestinationID = zDest.getText();
+    zOrigin.setText("");
+    zDest.setText("");
+
+    API api = new API();
+    try {
+      api.run(
+          (int) zOrigin.getScene().getWindow().getX(),
+          (int) zOrigin.getScene().getWindow().getY(),
+          (int) zOrigin.getScene().getWindow().getWidth(),
+          (int) zOrigin.getScene().getWindow().getHeight(),
+          "edu/wpi/DapperDaemons/assets/teamZAPI.css",
+          teamZDestinationID,
+          teamZOriginID);
+    } catch (Exception e) {
+      System.err.println("Team Z's API Broke");
+      return;
+    }
+    zErrorLabel.setText("You may have unsaved requests!");
+    zErrorLabel.setTextFill(Paint.valueOf("EF5353"));
+    zSave.setVisible(true);
   }
 
   /** Saves the external transport request from team Z's API */
   public void databaseSaverTeamZ() {
-    //    zSave.setVisible(false);
-    //    API teamZAPI = new API();
-    //    for (ExternalTransportRequest extReq : teamZAPI.getAllExternalTransportRequests()) {
-    //      if (!checkIfPatitentReqExists(
-    //          ExternalReqConverter.convert(extReq, teamZOriginID, teamZDestinationID)))
-    //        DAOPouch.getPatientTransportRequestDAO()
-    //            .add(ExternalReqConverter.convert(extReq, teamZOriginID, teamZDestinationID));
-    //    }
-    //    zErrorLabel.setText("Changes Saved!");
-    //    zErrorLabel.setTextFill(Paint.valueOf("00FF00"));
+    zSave.setVisible(false);
+    API teamZAPI = new API();
+    for (ExternalTransportRequest extReq : teamZAPI.getAllExternalTransportRequests()) {
+      if (!checkIfPatitentReqExists(
+          ExternalReqConverter.convert(extReq, teamZOriginID, teamZDestinationID)))
+        DAOPouch.getPatientTransportRequestDAO()
+            .add(ExternalReqConverter.convert(extReq, teamZOriginID, teamZDestinationID));
+    }
+    zErrorLabel.setText("Changes Saved!");
+    zErrorLabel.setTextFill(Paint.valueOf("00FF00"));
   }
 
   /**
