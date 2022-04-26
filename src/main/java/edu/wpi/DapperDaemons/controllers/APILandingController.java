a3b6a314f042ac630c55c0cd7e6f97b4a3918f2c
Samuel Karkache <samkarkachecollege@gmail.com>
2022-04-26 01:08:00 -0400

    connected employee databases

--- src/main/java/edu/wpi/DapperDaemons/controllers/APILandingController.java
+++ src/main/java/edu/wpi/DapperDaemons/controllers/APILandingController.java
@@ -4,9 +4,11 @@ import edu.wpi.DapperDaemons.APIConverters.ExternalReqConverter;
 import edu.wpi.DapperDaemons.APIConverters.InternalReqConverter;
 import edu.wpi.DapperDaemons.APIConverters.SanitationReqConverter;
 import edu.wpi.DapperDaemons.backend.DAOPouch;
+import edu.wpi.DapperDaemons.entities.Employee;
 import edu.wpi.DapperDaemons.entities.requests.PatientTransportRequest;
 import edu.wpi.DapperDaemons.entities.requests.SanitationRequest;
 import edu.wpi.cs3733.D22.teamB.api.DatabaseController;
+import edu.wpi.cs3733.D22.teamB.api.IPTEmployee;
 import edu.wpi.cs3733.D22.teamB.api.Request;
 import edu.wpi.cs3733.D22.teamD.API.*;
 import edu.wpi.cs3733.D22.teamD.entities.LocationObj;
@@ -65,6 +67,24 @@ public class APILandingController implements Initializable {
 
     // Team B API Init
     bSave.setVisible(false);
+    DatabaseController databaseController = new DatabaseController();
+    for (Employee employee : DAOPouch.getEmployeeDAO().getAll().values()) {
+        IPTEmployee empToAdd = new IPTEmployee(employee.getNodeID(),
+                employee.getLastName(),
+                employee.getFirstName(),
+                "Hospital Staff",
+                employee.getEmployeeType().toString()
+                );
+        if(!checkIfEmployeeExistsTeamB(empToAdd)) databaseController.add(empToAdd);
+    }
+  }
+  public boolean checkIfEmployeeExistsTeamB(IPTEmployee employee) {
+    DatabaseController databaseController = new DatabaseController();
+    for (IPTEmployee emp : databaseController.listEmployees()) {
+          if(emp.getFirstName().equals(employee.getFirstName())
+            && emp.getLastName().equals(employee.getLastName())) return true;
+    }
+    return false;
   }
 
   /** Allows for requests submitted by the API to be saved to our database */
