e1f1967a0af33ca25352a8ed2b34c93c670a00b2
Gibbeh <49353685+Gibbeh@users.noreply.github.com>
2022-04-26 21:05:43 -0400

    ready for backend

--- src/main/java/edu/wpi/DapperDaemons/controllers/NotificationsPageController.java
+++ src/main/java/edu/wpi/DapperDaemons/controllers/NotificationsPageController.java
@@ -4,11 +4,17 @@ import edu.wpi.DapperDaemons.entities.requests.Request;
 import edu.wpi.DapperDaemons.tables.Table;
 import edu.wpi.DapperDaemons.tables.TableHelper;
 import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class NotificationsPageController extends ParentController{
+    @FXML private VBox todayBox;
+    @FXML private VBox earlierBox;
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
